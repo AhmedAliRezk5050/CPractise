05a21e5a0332551a4e9fa1ea62bc1699c1608173
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-29 16:39:07 +0200

    Pointers pointer access stack memory (a variable)

--- CPractise/Source.cpp
+++ CPractise/Source.cpp
@@ -1,8 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+//
+// ---------------- Pointers ----------------
+//
 
+int main()
+{
+  int a = 10;
 
-int main() {
-	
+  int *p; // pointer initialization
+
+  p = &a; // pointer assignment
+
+  // %u: It is used to print unsigned decimal number
+  printf("%u\n", &a); // address of a
+
+  // %d: It is used to print signed decimal number
+  printf("%d\n", *p); //  dereferencing =>  value of a
 }
